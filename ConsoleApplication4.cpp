6e9bb087dd0e3430af6e823897230016b99aed40
Writer228 <stepan.makarenko.2012@mail.ru>
2017-03-09 13:24:08 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,49 @@
+// ConsoleApplication4.cpp: определяет точку входа для консольного приложения.
+//
+
+
+
+#include "Vector2.h"
+#include<iostream>
+
+int main()
+{
+	Vector2 a, b, a1, b1;
+	float x;
+	std::cin >> a;
+	std::cin >> b;
+	a1 = a;
+	b1 = b;
+	std::cout << a1 << std::endl;
+	std::cout << b1 << std::endl;
+	std::cout << a + b << std::endl;
+	std::cout << a - b << std::endl;
+	a1 += b;
+	std::cout << a1 << std::endl;
+	a1 = a;
+	a1 -= b;
+	std::cout << a1 << std::endl;
+	a1 = a;
+	x = a * b;
+	std::cout << x << std::endl;
+	x = a ^ b;
+	std::cout << x << std::endl;
+	std::cout << a * 2 << std::endl;
+	std::cout << 2 * a << std::endl;
+	a1 *= 2;
+	std::cout << a1 << std::endl;
+	a1 = a;
+	a1 /= 2;
+	std::cout << a1 << std::endl;
+	a1 = a;
+	std::cout << a / 2 << std::endl;
+	std::cout << a.norm() << std::endl;
+	std::cout << a.perpendicular() << std::endl;
+	std::cout << a.len() << std::endl;
+	std::cout << -a << std::endl;
+	a1.rotate(90);
+	std::cout << a1 << std::endl;
+	a1 = a;
+	a1 = a.getRotated(90);
+	std::cout << a1 << std::endl;
+}
